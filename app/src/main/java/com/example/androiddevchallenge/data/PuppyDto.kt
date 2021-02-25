9c2c6e82523fc7bf92e6f329fc47c5260b6d614b
roque.buarque <roque.buarque@share-now.com>
2021-02-25 21:20:47 +0100

    compose challenge

--- app/src/main/java/com/example/androiddevchallenge/data/PuppyDto.kt
+++ app/src/main/java/com/example/androiddevchallenge/data/PuppyDto.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge.data
 
 import androidx.annotation.DrawableRes
@@ -47,7 +62,8 @@ data class PuppyDto(
                     2,
                     R.drawable.ic_luna,
                     R.drawable.ic_luna_cover
-                ),        PuppyDto(
+                ),
+                PuppyDto(
                     3L,
                     "Juca",
                     "French Bulldog",
@@ -76,7 +92,8 @@ data class PuppyDto(
                     2,
                     R.drawable.ic_luna,
                     R.drawable.ic_luna_cover
-                ),        PuppyDto(
+                ),
+                PuppyDto(
                     6L,
                     "Juca",
                     "French Bulldog",
@@ -105,7 +122,8 @@ data class PuppyDto(
                     2,
                     R.drawable.ic_luna,
                     R.drawable.ic_luna_cover
-                ),        PuppyDto(
+                ),
+                PuppyDto(
                     9L,
                     "Juca",
                     "French Bulldog",
